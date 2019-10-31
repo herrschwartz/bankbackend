acb7eb08639033c3928e491562340547ff901cce
GRANVILLEHOMES\tschwartz <tschwartz@assemigroup.com>
2019-10-31 15:21:04 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/herrschwartz/bankbackend
+
+go 1.13
+
+require (
+	github.com/jackc/pgx v3.6.0+incompatible
+	github.com/jackc/pgx/v4 v4.1.2
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	golang.org/x/crypto v0.0.0-20191029031824-8986dd9e96cf // indirect
+	gopkg.in/inconshreveable/log15.v2 v2.0.0-20180818164646-67afb5ed74ec
+)
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"html/template"
+	"net/http"
+	"os"
+
+	"github.com/jackc/pgx/v4/log/log15adapter"
+	"github.com/jackc/pgx/v4/pgxpool"
+	log "gopkg.in/inconshreveable/log15.v2"
+)
+
+var db *pgxpool.Pool
+var tpl *template.Template
+
+//Item for things
+type Item struct {
+	Id  int
+	Amt int
+}
+
+type indexData struct {
+	Guild string
+	Items []Item
+}
+
+func urlHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == "GET" {
+		testdata := indexData{
+			Guild: "Its ok to be Whitemane",
+			Items: []Item{
+				{Id: 5113, Amt: 1},
+				{Id: 16204, Amt: 20},
+				{Id: 16393, Amt: 1},
+				{Id: 19019, Amt: 1},
+				{Id: 14551, Amt: 1},
+				{Id: 14552, Amt: 1},
+				{Id: 16223, Amt: 1},
+			},
+		}
+
+		tpl.ExecuteTemplate(w, "index.html", testdata)
+	}
+}
+
+func main() {
+	logger := log15adapter.NewLogger(log.New("module", "pgx"))
+
+	poolConfig, err := pgxpool.ParseConfig("postgres://postgres:admin@127.0.0.1:5432/postgres?pool_max_conns=10")
+	if err != nil {
+		log.Crit("Unable to parse DATABASE_URL", "error", err)
+		os.Exit(1)
+	}
+
+	poolConfig.ConnConfig.Logger = logger
+
+	db, err = pgxpool.ConnectConfig(context.Background(), poolConfig)
+	if err != nil {
+		log.Crit("Unable to create connection pool", "error", err)
+		os.Exit(1)
+	}
+
+	tpl = template.Must(template.ParseFiles("templates/index.html"))
+
+	http.HandleFunc("/", urlHandler)
+
+	log.Info("Starting URL shortener on localhost:8080")
+	err = http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		log.Crit("Unable to start web server", "error", err)
+		os.Exit(1)
+	}
+}
